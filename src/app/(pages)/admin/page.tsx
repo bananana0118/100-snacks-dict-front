d6054adb410307517558adbc4a9d9ed19f4d6db2
kyj <bananana0118@gmail.com>
2025-07-18 19:43:54 +0900

    [chore]: lint 적용

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,90 +1,20 @@
 'use client';
-import ImageUploader from '@/components/ImageUploader';
-import SelectList from '@/components/SelectList';
-import { Checkbox, Field, Input, Label } from '@headlessui/react';
+import CheckBoxWithLabel from '@/components/molecules/CheckBoxWithLabel';
+import ImageUploader from '@/components/molecules/ImageUploader';
+import SelectList from '@/components/molecules/SelectList';
+import { brandOptions } from '@/constants/Brand';
+import { Flavor, flavorOptions } from '@/constants/Flavors';
+import { priceRangeOptions } from '@/constants/PriceRange';
+import { snackOptions } from '@/constants/SnackCategory';
+import { storeOptions } from '@/constants/Store';
+import { Button, Field, Input, Label } from '@headlessui/react';
 import { useState } from 'react';
 
-// SnackCategory.ts
-export enum SnackCategory {
-  BagSnack = 'bagSnack', // 봉지과자
-  CrunchySnack = 'crunchySnack', // 바삭과자(비스킷)
-  SoftSnack = 'softSnack', // 폭신과자(파이)
-  Chocolate = 'chocolate', // 초콜릿
-  Jelly = 'jelly', // 젤리
-  CandyGum = 'candyGum', // 캔디&껌
-}
-
-// Store.ts
-export enum Store {
-  CU = 'CU', // 봉지과자
-  GS25 = 'GS25', // 바삭과자(비스킷)
-  SevenEleven = '세븐일레븐', // 폭신과자(파이)
-  Emart24 = '이마트24', // 초콜릿
-}
-
-// Brand.ts
-export enum Brand {
-  Lotte = '롯데제과', // 봉지과자
-  Olion = '오리온', // 바삭과자(비스킷)
-  Crown = '크라운', // 폭신과자(파이)
-  HaeTae = '해태', // 초콜릿
-}
-
-// PriceRange.ts
-export enum PriceRange {
-  OneToThreeThousand = '1000-3000', // 1,000원 ~ 3,000원
-  ThreeToFiveThousand = '3000-5000', // 3,000원 ~ 5,000원
-  FiveToTenThousand = '5000-10000', // 5,000원 ~ 10,000원
-  TenThousandAndAbove = '10000-', // 10,000원 이상
-}
-/** 1) Flavor enum & options 정의 */
-export enum Flavor {
-  Sweet = '달콤',
-  Tangy = '상큼',
-  Savory = '짭짤',
-  Spicy = '매콤',
-  Bitter = '쌉싸름',
-}
 // types.ts
 export interface Option<T extends string> {
   value: T;
   label: string;
 }
-
-export const storeOptions: Option<Store>[] = [
-  { value: Store.CU, label: 'CU' },
-  { value: Store.GS25, label: 'GS25' },
-  { value: Store.SevenEleven, label: '세븐일레븐' },
-  { value: Store.Emart24, label: '이마트24' },
-];
-
-export const brandOptions: Option<Brand>[] = [
-  { value: Brand.Lotte, label: '롯데제과' },
-  { value: Brand.Olion, label: '오리온' },
-  { value: Brand.Crown, label: '크라운' },
-  { value: Brand.HaeTae, label: '해태' },
-];
-
-const snackOptions: Option<SnackCategory>[] = [
-  { value: SnackCategory.BagSnack, label: '봉지과자' },
-  { value: SnackCategory.CrunchySnack, label: '바삭과자(비스킷)' },
-  { value: SnackCategory.SoftSnack, label: '폭신과자(파이)' },
-  { value: SnackCategory.Chocolate, label: '초콜릿' },
-  { value: SnackCategory.Jelly, label: '젤리' },
-  { value: SnackCategory.CandyGum, label: '캔디&껌' },
-];
-
-export const priceRangeOptions: Option<PriceRange>[] = [
-  { value: PriceRange.OneToThreeThousand, label: '1,000원 ~ 3,000원' },
-  { value: PriceRange.ThreeToFiveThousand, label: '3,000원 ~ 5,000원' },
-  { value: PriceRange.FiveToTenThousand, label: '5,000원 ~ 10,000원' },
-  { value: PriceRange.TenThousandAndAbove, label: '10,000원 이상' },
-];
-
-const flavorOptions = Object.values(Flavor).map((flavor) => ({
-  value: flavor,
-  label: flavor,
-}));
 
 const Page = () => {
   const [selected, setSelected] = useState<Flavor[]>([]);
@@ -97,6 +27,35 @@ const Page = () => {
     }
   };
 
+  const onSubmitHandler: React.FormEventHandler<HTMLFormElement> = async (
+    e,
+  ) => {
+    e.preventDefault();
+
+    // 여기서 e.currentTarget이 form 엘리먼트
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    // 예시: 모든 필드 찍어보기
+    for (const [key, val] of formData.entries()) {
+      console.log(key, val);
+    }
+
+    const flavors = formData.getAll('flavor') as string[];
+    console.log(flavors);
+
+    // 3) 이미지 파일
+    const file = formData.get('image') as File | null;
+    console.log('file:', file);
+
+    // const res = await fetch('/api/snack', {
+    //   method: 'POST',
+    //   body: fd,
+    // });
+    // const json = await res.json();
+    // console.log('서버 응답:', json);
+  };
+
   return (
     <div>
       <main className="flex flex-col items-center justify-center px-[50px] pt-[24px] pb-[50px]">
@@ -104,7 +63,7 @@ const Page = () => {
           <p className="mb-2.5 flex items-center justify-center text-[28px] font-semibold">
             과자등록
           </p>
-          <form className="flex flex-col gap-6">
+          <form className="flex flex-col gap-6" onSubmit={onSubmitHandler}>
             <ImageUploader></ImageUploader>
             <Field className="flex flex-col">
               <Label className="pb-2.5 text-lg font-semibold">1. 과자명</Label>
@@ -144,38 +103,15 @@ const Page = () => {
                   const isChecked = selected.includes(opt.value);
                   const isDisabled = !isChecked && selected.length >= 4;
                   return (
-                    <Field
-                      as="div"
+                    <CheckBoxWithLabel
                       key={opt.value}
-                      disabled={isDisabled}
-                      className="flex items-center"
-                    >
-                      <Checkbox
-                        className={`group mr-1 block h-5 w-5 rounded border bg-white data-checked:bg-blue-500 data-disabled:cursor-not-allowed data-disabled:opacity-50`}
-                        onChange={() => toggleFlavor(opt.value)}
-                        value={opt.value}
-                        checked={isChecked}
-                        disabled={isDisabled}
-                      >
-                        <svg
-                          viewBox="0 0 14 14"
-                          fill="none"
-                          className="stroke-white opacity-0 group-data-checked:opacity-100"
-                        >
-                          <path
-                            d="M3 8L6 11L11 3.5"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </Checkbox>
-                      <Label
-                        className={isDisabled ? 'text-gray-400' : undefined}
-                      >
-                        {opt.label}
-                      </Label>
-                    </Field>
+                      isChecked={isChecked}
+                      isDisabled={isDisabled}
+                      onChangeHandler={() => toggleFlavor(opt.value)}
+                      value={opt.value}
+                      name="flavor"
+                      label={opt.label}
+                    ></CheckBoxWithLabel>
                   );
                 })}
               </div>
@@ -209,6 +145,12 @@ const Page = () => {
                 ></Input>
               </div>
             </Field>
+            <Button
+              type="submit"
+              className="h-15 w-full cursor-pointer bg-[#2E1515] px-2.5 py-2.5 text-xl font-semibold text-white"
+            >
+              등록
+            </Button>
           </form>
         </section>
       </main>
