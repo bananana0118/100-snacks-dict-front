f0f446c04713c80ff551228e47d7748b6615c484
bananana0118 <bananana0118@gmail.com>
2025-08-04 22:18:23 +0900

    [feat]: update snackListItem add icon

--- src/components/molecules/SnackListItem.tsx
+++ src/components/molecules/SnackListItem.tsx
@@ -1,19 +1,22 @@
 import { TASTE_CODE_TO_NAME } from '@/constants/Tastes';
 import { SnackItem } from '@/services/snack/sncakService';
+import { Circle } from 'lucide-react';
 
 type SnackListItemProps = {
   snack: SnackItem;
+  isHover: boolean;
 };
 
-const SnackListItem = ({ snack }: SnackListItemProps) => {
+const SnackListItem = ({ snack, isHover }: SnackListItemProps) => {
   const { id } = snack;
   return (
     <li
       key={id}
       className="flex h-6 items-center justify-between gap-2.5 text-xl"
     >
-      <div>
-        icon<span>{snack.name}</span>
+      <div className="flex flex-row items-center gap-2.5">
+        <Circle size={16} fill={isHover ? '#000' : '#fff'} />
+        <span>{snack.name}</span>
       </div>
       <hr className="flex w-1 flex-1"></hr>
       <div>
