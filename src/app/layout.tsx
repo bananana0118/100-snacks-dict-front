10703afec8ff4a011375a03e26bed2fe7a93ddb8
bananana0118 <bananana0118@gmail.com>
2025-08-09 13:17:21 +0900

    [feat]: 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import './globals.css';
+import localFont from 'next/font/local';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
+});
+
+const pretendard = localFont({
+  src: './font/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -18,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={pretendard.className}>
       <body
         className={`${geistSans.variable} mx-auto max-w-[1980px] antialiased`}
       >
