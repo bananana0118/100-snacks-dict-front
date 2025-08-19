00b911a1bfb310c353a564718487e380ca761836
bananana0118 <bananana0118@gmail.com>
2025-08-19 22:18:27 +0900

    [chore]: 안쓰는 코드 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist } from 'next/font/google';
 import './globals.css';
 import localFont from 'next/font/local';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
 
 const pretendard = localFont({
   src: './font/PretendardVariable.woff2',
